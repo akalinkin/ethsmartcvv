302a6fb901357f1f525f9b11a42e34eb847990aa
Alex Kalinkin <alex@kalinkin.info>
2017-06-26 18:04:00 +0300

    React app added as future DApp UI and contract interop

---
+++ dapp/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>DApp for EthSmartCvv contract</h2>
+        </div>
+        <p className="App-intro">
+          Info will be here.
+
+          Form which will ask smart contract about current treasure cost and
+          button toggled to "Buy" if current user was not payed before or
+          toggled to "Get" if current user already payed
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
