0443f8762409dda484a428fc7b1f51a52d44bf25
Alex Kalinkin <alex@kalinkin.info>
2017-06-27 18:53:54 +0300

    Add few tests

--- test/ValueOwners.js
+++ test/ValueOwners.js
@@ -42,4 +42,46 @@ contract('ValueOwners', function(accounts) {
       assert.equal(amount, 0,'Amount isn\'t 0')
     });
   });
+
+  it("should return true if payed 10000000000000000 Wei", function() {
+    var vo_contract;
+    var account_one = accounts[0];
+
+    return ValueOwners.deployed().then(function(instance) {
+      vo_contract = instance;
+      return instance.buyValue.call({from: account_one, value:10000000000000000});
+    }).then(function(success) {
+      assert.equal(success, true,'buyValue doesn\'t returns true');
+    });
+  });
+
+  /*it("should return false when not correct amount of Ether sended", function() {
+    var account_one = accounts[0];
+
+    return ValueOwners.deployed().then(function(instance) {
+      return instance.buyValue.call({from: account_one, value:10000000000000001});
+    }).then(function(success) {
+      assert.equal(success,false,'buyValue doesn\'t return false');
+    });
+  });*/
+
+  // TODO: Find out how to wait while first transaction will be applied to new block and only after that try to call method second time
+  /*it("should return false if payed 10000000000000000 Wei double times from one account", function() {
+    var vo_contract;
+    var account_one = accounts[0];
+    //console.log(web3.fromWei(account_one, "ether"));
+
+    return ValueOwners.deployed().then(function(instance) {
+      vo_contract = instance;
+      return vo_contract.buyValue.call({from: account_one, value:10000000000000000});
+    }).then(function(success) {
+      assert.equal(success, true, 'buyValue doesn\'t returns true first time');
+    }).then(function() {
+      return vo_contract.buyValue.call({from: account_one,
+      value:10000000000000000});
+    }).then(function(success) {
+      assert.equal(success, false, 'buyValue doesn\'t returns false second time');
+    });
+  });*/
+
 });
