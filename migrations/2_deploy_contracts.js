d95fc8ce82397199e767573f50b0164f12b894ea
Alex Kalinkin <alex@kalinkin.info>
2017-06-27 11:48:52 +0300

    Fix migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+var OwnedMortal = artifacts.require("./OwnedMortal.sol");
 var ValueOwners = artifacts.require("./ValueOwners.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(OwnedMortal);
+  deployer.link(OwnedMortal, ValueOwners);
   deployer.deploy(ValueOwners);
 };
